b96b4d7a7899a710ec35e3e82910dd803039cb5b
bryan <bryan.fagundes@crearesistemas.com.br>
2025-02-10 22:28:54 -0300

    inclusão de arquivos

--- script.js
+++ script.js
@@ -8,3 +8,13 @@
         
             window.open(linkWhatsApp, "_blank");
         }
+
+        function nokPresenca() {
+            
+            const numero = "5551995017229"; 
+            const mensagem = "Olá, não vou conseguir comparecer ao aniversário!";
+            
+            const linkWhatsApp = `https://wa.me/${numero}?text=${encodeURIComponent(mensagem)}`;
+        
+            window.open(linkWhatsApp, "_blank");
+        }
